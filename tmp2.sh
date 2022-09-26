31121fcc0bc2c16b76ffbbd6a0d9501d1df185f0
Michael Majurski <mmajurski@gmail.com>
2022-09-25 20:09:26 -0400

    setup cluster

---
+++ tmp2.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+# **************************
+# MODIFY THESE OPTIONS
+
+#SBATCH --partition=heimdall
+#SBATCH --nodes=1
+#SBATCH --nice
+#SBATCH --oversubscribe
+#SBATCH --cpus-per-task=16
+#SBATCH --gres=gpu:1
+#SBATCH --job-name=gmm
+#SBATCH -o log-%N.%j.out
+#SBATCH --time=96:0:0
+
+source /mnt/isgnas/home/mmajursk/anaconda3/etc/profile.d/conda.sh
+conda activate gmm
+
+i=0
+n=250
+c=2
+
+python main.py --output-filepath=./models/ssl-resp-n${n}-c${c}-models/id-000${i} --num_labeled_datapoints=${n} --pseudo-label-percentile-threshold="resp" --cluster_per_class=${c} &
+sleep 0.2
+
+python main.py --output-filepath=./models/ssl-neum-n${n}-c${c}-models/id-000${i} --num_labeled_datapoints=${n} --pseudo-label-percentile-threshold="neum" --cluster_per_class=${c} &
+sleep 0.2
+
+i=1
+
+python main.py --output-filepath=./models/ssl-resp-n${n}-c${c}-models/id-000${i} --num_labeled_datapoints=${n} --pseudo-label-percentile-threshold="resp" --cluster_per_class=${c}&
+sleep 0.2
+
+python main.py --output-filepath=./models/ssl-neum-n${n}-c${c}-models/id-000${i} --num_labeled_datapoints=${n} --pseudo-label-percentile-threshold="neum" --cluster_per_class=${c} &
+sleep 0.2
+
+
+# Baselines (no SSL)
+for c in 2 4 8; do
+  for n in 250 1000 4000; do
+    for i in {0..4}; do
+      wait -n
+      python main.py --output-filepath=./models/ssl-resp-n${n}-c${c}-models/id-000${i} --num_labeled_datapoints=${n} --pseudo-label-percentile-threshold="resp" --cluster_per_class=${c} &
+      sleep 0.2
+
+      wait -n
+      python main.py --output-filepath=./models/ssl-neum-n${n}-c${c}-models/id-000${i} --num_labeled_datapoints=${n} --pseudo-label-percentile-threshold="neum" --cluster_per_class=${c} &
+      sleep 0.2
+
+      # ***********************
+
+      wait -n
+      python main.py --output-filepath=./models/ssl-resp-cauchy-n${n}-c${c}-models/id-000${i} --num_labeled_datapoints=${n} --pseudo-label-percentile-threshold="resp" --cluster_per_class=${c} --inference-method=cauchy &
+      sleep 0.2
+
+      wait -n
+      python main.py --output-filepath=./models/ssl-neum-cauchy-n${n}-c${c}-models/id-000${i} --num_labeled_datapoints=${n} --pseudo-label-percentile-threshold="neum" --cluster_per_class=${c} --inference-method=cauchy &
+      sleep 0.2
+
+
+      # ***********************
+      for p in "0.99" "0.98" "0.95" "0.9" "0.75"; do
+        wait -n
+        python main.py --output-filepath=./models/ssl-perc${p}-n${n}-c${c}-models/id-000${i} --num_labeled_datapoints=${n} --pseudo-label-percentile-threshold=${p} --cluster_per_class=${c} &
+        sleep 0.2
+      done
+
+      # ***********************
+
+      for p in "0.99" "0.98" "0.95" "0.9" "0.75"; do
+        wait -n
+        python main.py --output-filepath=./models/ssl-cauchy-perc${p}-n${n}-c${c}-models/id-000${i} --num_labeled_datapoints=${n} --pseudo-label-percentile-threshold=${p} --cluster_per_class=${c} --inference-method=cauchy &
+        sleep 0.2
+      done
+
+    done
+  done
+done
+
+wait
+
