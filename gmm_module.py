Fix: Cauchy calculation mistake, Fixed order in all functions (skl_cauchy_mm, gmm_module)

gmm_module.py
@@ -239,7 +239,7 @@ class GMM(torch.nn.Module):
             x = torch.tensor(x)
 
         unnorm_log_resp, log_prob_norm, log_resp = self._cauchy_estimate_log_prob_resp(x)
-        return unnorm_log_resp, torch.exp(log_prob_norm), torch.exp(log_resp)
+        return torch.exp(unnorm_log_resp), torch.exp(log_prob_norm), torch.exp(log_resp)
 
     def _compute_precision_cholesky(self):
         """

train_ssl.py
@@ -471,7 +471,7 @@ def eval_model(model, pytorch_dataset, criterion, train_stats, split_name, epoch
                     gmm_preds.extend(gmm_pred)
 
                     if hasattr(gmm, 'predict_cauchy_probability'):
-                        _, cauchy_resp, cauchy_unnorm_resp = gmm.predict_cauchy_probability(gmm_inputs)
+                        cauchy_unnorm_resp, _, cauchy_resp = gmm.predict_cauchy_probability(gmm_inputs)
                         if not isinstance(cauchy_resp, np.ndarray):
                             cauchy_resp = cauchy_resp.detach().cpu().numpy()
                         cauchy_pred = np.argmax(cauchy_resp, axis=-1)